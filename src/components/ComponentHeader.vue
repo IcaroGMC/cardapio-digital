43ee9f60741a93880624e61dc2a5737fd3f9c79f
IcaroGMC <icarogabrielchaves@outlook.com.br>
2022-11-18 16:56:52 -0300

    Modificação na cor do placeholder

--- src/components/ComponentHeader.vue
+++ src/components/ComponentHeader.vue
@@ -80,6 +80,11 @@ export default {
                                 border: none;
                             }
                         }
+                        
+                        ::placeholder {
+                            font-weight: 400;
+                            color: #343A40;
+                        }
 
                         button {
                             width: 16px; height: 16px;
